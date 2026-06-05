5072b7b7931308d04b2e57be078a618a0d74e30b
Alpheus <26622568+alpheusmtx@users.noreply.github.com>
2026-06-06 04:28:13 +0800

    feat: code splitting for create virtual entry code

--- packages/vite/src/plugins/build/index.ts
+++ packages/vite/src/plugins/build/index.ts
@@ -18,6 +18,45 @@ const VIRTUAL_ENTRY = "virtual:srvkit" as const;
 
 const VIRTUAL_ENTRY_RESOLVED = `\0${VIRTUAL_ENTRY}` as const;
 
+const createVirtualEntryCode = (opts: ResolvedOptions): string => {
+    const build: ResolvedBuildOptions = opts.build;
+
+    let code: string = "";
+
+    code += `import options from "${toPosix(opts.entry)}";`;
+    code += `import { serve } from "@srvkit/vite/runtime";`;
+
+    // handler export
+
+    if (build.target === "handler") {
+        code += `const server = serve({ ...options, manual: true });`;
+        code += `export default server;`;
+
+        return code;
+    }
+
+    // server export
+
+    code += `serve({`;
+    code += `...options,`;
+
+    if (build.host !== "localhost") code += `hostname: "${build.host}",`;
+    if (build.port !== 3000) code += `port: ${build.port},`;
+
+    if (build.https) {
+        code += `tls: {`;
+        if (build.https.cert) code += `cert: "${toPosix(build.https.cert)}",`;
+        if (build.https.key) code += `key: "${toPosix(build.https.key)}",`;
+        if (build.https.passphrase)
+            code += `passphrase: "${toPosix(build.https.passphrase)}",`;
+        code += `},`;
+    }
+
+    code += `});`;
+
+    return code;
+};
+
 const buildPlugin = (opts: ResolvedOptions): Plugin => {
     const build: ResolvedBuildOptions = opts.build;
 
@@ -100,44 +139,7 @@ const buildPlugin = (opts: ResolvedOptions): Plugin => {
         },
         load: async (id: string): Promise<LoadResult> => {
             if (id !== VIRTUAL_ENTRY_RESOLVED) return void 0;
-
-            let code: string = "";
-
-            code += `import options from "${toPosix(opts.entry)}";`;
-            code += `import { serve } from "@srvkit/vite/runtime";`;
-
-            // handler export
-
-            if (build.target === "handler") {
-                code += `const server = serve({ ...options, manual: true });`;
-                code += `export default server;`;
-
-                return code;
-            }
-
-            // server export
-
-            code += `serve({`;
-            code += `...options,`;
-
-            if (build.host !== "localhost")
-                code += `hostname: "${build.host}",`;
-            if (build.port !== 3000) code += `port: ${build.port},`;
-
-            if (build.https) {
-                code += `tls: {`;
-                if (build.https.cert)
-                    code += `cert: "${toPosix(build.https.cert)}",`;
-                if (build.https.key)
-                    code += `key: "${toPosix(build.https.key)}",`;
-                if (build.https.passphrase)
-                    code += `passphrase: "${toPosix(build.https.passphrase)}",`;
-                code += `},`;
-            }
-
-            code += `});`;
-
-            return code;
+            return createVirtualEntryCode(opts);
         },
     };
 };
